a81b29172547d5eb5bbc11b80ff4b9fcfca764aa
mdahadahmed908-ctrl <mdahadahmed908@gmail.com>
2026-08-02 14:43:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Tkbd9999 Loaded Successfully");
+
+// Search
+const search = document.querySelector(".search-box input");
+
+search.addEventListener("keyup", function () {
+  const value = this.value.toLowerCase();
+  const games = document.querySelectorAll(".game-card");
+
+  games.forEach(game => {
+    const name = game.querySelector("h3").innerText.toLowerCase();
+
+    if (name.includes(value)) {
+      game.style.display = "block";
+    } else {
+      game.style.display = "none";
+    }
+  });
+});
+
+// Play Button
+document.querySelectorAll(".game-card button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Game feature coming soon!");
+  });
+});
+
+// Bottom Navigation
+document.querySelectorAll(".nav").forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".nav").forEach(n => n.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
